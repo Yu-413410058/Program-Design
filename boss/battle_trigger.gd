71dd1741305880a727e118b5ac2b22247408e150
Yu-413410058 <alum413410058@alum.ccu.edu.tw>
2025-06-01 04:51:13 +0800

    add music
    yeah i added some music

--- boss/battle_trigger.gd
+++ boss/battle_trigger.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 var player_inside: = false
+@onready var battlemusic: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _on_body_entered(body):
+	if not battlemusic.playing:
+		battlemusic.play()
 	if body.is_in_group("player"):
 		player_inside = true
 		print("Player entered battle area")
