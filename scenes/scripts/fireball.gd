671b8f5f5f73950949d504c393cee6c1d2936fc6
Yu-413410058 <alum413410058@alum.ccu.edu.tw>
2025-06-01 16:53:43 +0800

    updated victory screen

--- scenes/scripts/fireball.gd
+++ scenes/scripts/fireball.gd
@@ -5,16 +5,16 @@ extends Area2D
 
 
 func _ready() -> void :
-    await get_tree().create_timer(lifetime).timeout
-    queue_free()
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
 
 
 func _process(delta: float) -> void :
-    global_position.x += speed * delta * (1 if scale.x > 0 else -1)
+	global_position.x += speed * delta * (1 if scale.x > 0 else -1)
 
 func _on_area_entered(area):
-    if area.is_in_group("enemies"):
-        area.on_hit_by_fireball()
-    elif area.is_in_group("flame"):
-        return
-    queue_free()
+	if area.is_in_group("enemies"):
+		area.on_hit_by_fireball()
+	elif area.is_in_group("flame"):
+		return
+	queue_free()
